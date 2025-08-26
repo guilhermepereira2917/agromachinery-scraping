58cd89d35728a19cd1f11665e6460e417940fe91
Guilherme Pereira <guilhermepereira2917@gmail.com>
2025-08-26 02:22:07 -0300

    feat: add jackson object mapper and jsoup dependencies

---
+++ src/main/java/br/com/oystr/agromachinery/scraping/config/ObjectMapperConfig.java
@@ -0,0 +1,15 @@
+package br.com.oystr.agromachinery.scraping.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ObjectMapperConfig {
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
